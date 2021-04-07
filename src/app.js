ecbdafb9699a2a3fc837d2258eb154b1f2031df4
Christine Yoo <christinecho.s@gmail.com>
2021-04-07 16:51:04 -0700

    installed winston

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const winston = require("winston");
 
 const app = express();
 
@@ -12,6 +13,20 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+})
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }))
+}
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
